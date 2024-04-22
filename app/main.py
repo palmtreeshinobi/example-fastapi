98620dfde5e1e367674d64fc2af10e447fe64f59
Van <89276523+palmtreeshinobi@users.noreply.github.com>
2024-04-22 14:42:58 -0400

    push to Heroku example

--- app/main.py
+++ app/main.py
@@ -28,4 +28,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Lazy Palms API practice"}
+    return {"message": "Successfully deployed from CI/CD Pipeline"}
